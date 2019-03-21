2c180f67a39b3324a09f23f7546878a8bdccb600
zhuimenran <2161434564@qq.com>
2019-03-21 22:52:17 +0800

    updata twosum

--- src/com/txb/day1/io/TwoSumIo.java
+++ src/com/txb/day1/io/TwoSumIo.java
@@ -1,5 +1,9 @@
 package com.txb.day1.io;
-
+/**
+ * 处理求得两个数的索引的IO
+ * @author 13125
+ * @time 2019.3.21
+ */
 public class TwoSumIo {
 
 }
--- src/com/txb/day1/java/TwoSumDemo.java
+++ src/com/txb/day1/java/TwoSumDemo.java
@@ -1,5 +1,25 @@
 package com.txb.day1.java;
-
-public class TwoSumDemo {
-
+/**
+ * 给定一个整数数组，返回两个数字的索引，使它们相加到特定目标
+ * @author 13125
+ * @time 2019.3.21
+ */
+public class  TwoSumDemo {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        int j = 0;
+        
+        for(i=0; i<nums.length; i++){
+            for(j=i+1; j<nums.length;j++){
+                if((nums[i]+nums[j])==target){
+                    if(i!=j){  
+                        return new int[] { i, j };
+                    }else{
+                        ;
+                    }
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 }
