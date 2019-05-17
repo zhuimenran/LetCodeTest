881f6dd2ccc663fc786f9593412597014b615dda
zhuimenran <13125110952@163.com>
2019-05-17 17:55:48 +0800

    updata

--- src/com/txb/day1/io/TwoSumIo.java
+++ src/com/txb/day1/io/TwoSumIo.java
@@ -5,5 +5,19 @@ package com.txb.day1.io;
  * @time 2019.3.21
  */
 public class TwoSumIo {
-
+	int nums [] = null;
+	static int NUMS_NUM = 5;
+	/*
+	 * input:nums[n], target
+	 * outpu:num[2]
+	 */
+	public boolean input() {
+		System.out.println("input");
+		
+		
+		
+		
+		return true;
+	}
+		
 }
